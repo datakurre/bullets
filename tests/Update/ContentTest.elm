7d7e2dc8fd408e14249407c6dac1642d06047ede
Asko Soukka <asko.soukka@iki.fi>
2025-12-17 12:14:42 +0200

    Refactor Update/ContentTest.elm to BDD style
    - Add nested describe blocks for given-when-then structure
    - Use 'should' pattern for test names
    - Group related tests with clear behavior descriptions
    - Improves test readability and intent

--- tests/Update/ContentTest.elm
+++ tests/Update/ContentTest.elm
@@ -32,71 +32,83 @@ suite : Test
 suite =
     describe "Update.Content"
         [ describe "updateContent"
-            [ test "updates editingContent buffer" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ defaultSlide ]
+            [ describe "given a presentation with one slide"
+                [ describe "when updating content"
+                    [ test "should update the editingContent buffer" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ defaultSlide ]
 
-                        ( newModel, _ ) =
-                            Update.Content.updateContent "# Updated" model
-                    in
-                    Expect.equal "# Updated" newModel.editingContent
-            , test "updates current slide content" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ defaultSlide, defaultSlide, defaultSlide ]
-                                |> (\m -> { m | currentSlideIndex = 1 })
+                                ( newModel, _ ) =
+                                    Update.Content.updateContent "# Updated" model
+                            in
+                            Expect.equal "# Updated" newModel.editingContent
+                    ]
+                ]
+            , describe "given a presentation with multiple slides"
+                [ describe "when updating content of the second slide"
+                    [ test "should update the current slide content" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ defaultSlide, defaultSlide, defaultSlide ]
+                                        |> (\m -> { m | currentSlideIndex = 1 })
 
-                        ( newModel, _ ) =
-                            Update.Content.updateContent "# New Content" model
+                                ( newModel, _ ) =
+                                    Update.Content.updateContent "# New Content" model
 
-                        currentSlide =
-                            List.drop 1 newModel.presentation.slides
-                                |> List.head
-                                |> Maybe.map .content
-                    in
-                    Expect.equal (Just "# New Content") currentSlide
-            , test "does not update other slides" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ defaultSlide, defaultSlide, defaultSlide ]
-                                |> (\m -> { m | currentSlideIndex = 1 })
+                                currentSlide =
+                                    List.drop 1 newModel.presentation.slides
+                                        |> List.head
+                                        |> Maybe.map .content
+                            in
+                            Expect.equal (Just "# New Content") currentSlide
+                    , test "should not update other slides" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ defaultSlide, defaultSlide, defaultSlide ]
+                                        |> (\m -> { m | currentSlideIndex = 1 })
 
-                        ( newModel, _ ) =
-                            Update.Content.updateContent "# New Content" model
+                                ( newModel, _ ) =
+                                    Update.Content.updateContent "# New Content" model
 
-                        firstSlide =
-                            List.head newModel.presentation.slides
-                                |> Maybe.map .content
-                    in
-                    Expect.equal (Just "# Original") firstSlide
+                                firstSlide =
+                                    List.head newModel.presentation.slides
+                                        |> Maybe.map .content
+                            in
+                            Expect.equal (Just "# Original") firstSlide
+                    ]
+                ]
             ]
         , describe "updateTitle"
-            [ test "updates presentation title" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ defaultSlide ]
+            [ describe "given a presentation"
+                [ describe "when updating the title"
+                    [ test "should update the presentation title" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ defaultSlide ]
 
-                        ( newModel, _ ) =
-                            Update.Content.updateTitle "New Title" model
-                    in
-                    Expect.equal "New Title" newModel.presentation.title
-            , test "does not affect slides" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ defaultSlide ]
+                                ( newModel, _ ) =
+                                    Update.Content.updateTitle "New Title" model
+                            in
+                            Expect.equal "New Title" newModel.presentation.title
+                    , test "should not affect slides" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ defaultSlide ]
 
-                        ( newModel, _ ) =
-                            Update.Content.updateTitle "New Title" model
+                                ( newModel, _ ) =
+                                    Update.Content.updateTitle "New Title" model
 
-                        slideCount =
-                            List.length newModel.presentation.slides
-                    in
-                    Expect.equal 1 slideCount
+                                slideCount =
+                                    List.length newModel.presentation.slides
+                            in
+                            Expect.equal 1 slideCount
+                    ]
+                ]
             ]
         ]
