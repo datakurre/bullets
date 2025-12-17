ee062e64d3ee5aa21d46f33db73261c930c52f3f
Asko Soukka <asko.soukka@iki.fi>
2025-12-17 12:29:54 +0200

    Refactor Update/ImageTest.elm to BDD style
    - Add nested describe blocks for given-when-then structure
    - Use 'should' pattern for test names
    - Group tests by image operations (paste, load, remove)
    - Improves test readability and intent

--- tests/Update/ImageTest.elm
+++ tests/Update/ImageTest.elm
@@ -36,108 +36,124 @@ suite : Test
 suite =
     describe "Update.Image"
         [ describe "imagePasted"
-            [ test "adds image to current slide" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ defaultSlide, defaultSlide ]
-                                |> (\m -> { m | currentSlideIndex = 1 })
+            [ describe "given a presentation with multiple slides"
+                [ describe "when pasting an image to a slide"
+                    [ test "should add the image to the current slide" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ defaultSlide, defaultSlide ]
+                                        |> (\m -> { m | currentSlideIndex = 1 })
 
-                        dataUri =
-                            "data:image/png;base64,test"
+                                dataUri =
+                                    "data:image/png;base64,test"
 
-                        ( newModel, _ ) =
-                            Update.Image.imagePasted dataUri model
+                                ( newModel, _ ) =
+                                    Update.Image.imagePasted dataUri model
 
-                        currentSlide =
-                            List.drop 1 newModel.presentation.slides
-                                |> List.head
-                    in
-                    case currentSlide of
-                        Just slide ->
-                            Expect.equal (Just dataUri) slide.image
+                                currentSlide =
+                                    List.drop 1 newModel.presentation.slides
+                                        |> List.head
+                            in
+                            case currentSlide of
+                                Just slide ->
+                                    Expect.equal (Just dataUri) slide.image
 
-                        Nothing ->
-                            Expect.fail "Expected slide to exist"
-            , test "does not affect other slides" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ defaultSlide, defaultSlide, defaultSlide ]
-                                |> (\m -> { m | currentSlideIndex = 1 })
+                                Nothing ->
+                                    Expect.fail "Expected slide to exist"
+                    , test "should not affect other slides" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ defaultSlide, defaultSlide, defaultSlide ]
+                                        |> (\m -> { m | currentSlideIndex = 1 })
 
-                        ( newModel, _ ) =
-                            Update.Image.imagePasted "data:image/png;base64,test" model
+                                ( newModel, _ ) =
+                                    Update.Image.imagePasted "data:image/png;base64,test" model
 
-                        firstSlide =
-                            List.head newModel.presentation.slides
-                    in
-                    case firstSlide of
-                        Just slide ->
-                            Expect.equal Nothing slide.image
+                                firstSlide =
+                                    List.head newModel.presentation.slides
+                            in
+                            case firstSlide of
+                                Just slide ->
+                                    Expect.equal Nothing slide.image
 
-                        Nothing ->
-                            Expect.fail "Expected slide to exist"
+                                Nothing ->
+                                    Expect.fail "Expected slide to exist"
+                    ]
+                ]
             ]
         , describe "imageFileLoaded"
-            [ test "sets image on current slide" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ defaultSlide ]
+            [ describe "given a slide"
+                [ describe "when loading an image from a file"
+                    [ test "should set the image on the current slide" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ defaultSlide ]
 
-                        dataUri =
-                            "data:image/png;base64,loaded"
+                                dataUri =
+                                    "data:image/png;base64,loaded"
 
-                        ( newModel, _ ) =
-                            Update.Image.imageFileLoaded dataUri model
+                                ( newModel, _ ) =
+                                    Update.Image.imageFileLoaded dataUri model
 
-                        currentSlide =
-                            List.head newModel.presentation.slides
-                    in
-                    case currentSlide of
-                        Just slide ->
-                            Expect.equal (Just dataUri) slide.image
+                                currentSlide =
+                                    List.head newModel.presentation.slides
+                            in
+                            case currentSlide of
+                                Just slide ->
+                                    Expect.equal (Just dataUri) slide.image
 
-                        Nothing ->
-                            Expect.fail "Expected slide to exist"
+                                Nothing ->
+                                    Expect.fail "Expected slide to exist"
+                    ]
+                ]
             ]
         , describe "removeImage"
-            [ test "removes image from current slide" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ slideWithImage ]
+            [ describe "given a slide with an image"
+                [ describe "when removing the image"
+                    [ test "should remove the image from the current slide" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ slideWithImage ]
 
-                        ( newModel, _ ) =
-                            Update.Image.removeImage model
+                                ( newModel, _ ) =
+                                    Update.Image.removeImage model
 
-                        currentSlide =
-                            List.head newModel.presentation.slides
-                    in
-                    case currentSlide of
-                        Just slide ->
-                            Expect.equal Nothing slide.image
+                                currentSlide =
+                                    List.head newModel.presentation.slides
+                            in
+                            case currentSlide of
+                                Just slide ->
+                                    Expect.equal Nothing slide.image
 
-                        Nothing ->
-                            Expect.fail "Expected slide to exist"
-            , test "does not affect slides without images" <|
-                \_ ->
-                    let
-                        model =
-                            modelWithSlides [ defaultSlide ]
+                                Nothing ->
+                                    Expect.fail "Expected slide to exist"
+                    ]
+                ]
+            , describe "given a slide without an image"
+                [ describe "when attempting to remove an image"
+                    [ test "should not affect the slide" <|
+                        \_ ->
+                            let
+                                model =
+                                    modelWithSlides [ defaultSlide ]
 
-                        ( newModel, _ ) =
-                            Update.Image.removeImage model
+                                ( newModel, _ ) =
+                                    Update.Image.removeImage model
 
-                        currentSlide =
-                            List.head newModel.presentation.slides
-                    in
-                    case currentSlide of
-                        Just slide ->
-                            Expect.equal Nothing slide.image
+                                currentSlide =
+                                    List.head newModel.presentation.slides
+                            in
+                            case currentSlide of
+                                Just slide ->
+                                    Expect.equal Nothing slide.image
 
-                        Nothing ->
-                            Expect.fail "Expected slide to exist"
+                                Nothing ->
+                                    Expect.fail "Expected slide to exist"
+                    ]
+                ]
             ]
         ]
