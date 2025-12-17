03449f8c875fe40420f2cd79734ccb5e993ad4ea
Asko Soukka <asko.soukka@iki.fi>
2025-12-17 12:37:47 +0200

    Refactor Update/FileIOTest.elm to BDD style
    - Add nested describe blocks for given-when-then structure
    - Use 'should' pattern for test names
    - Group tests by file operations (export, import)
    - Improves test readability and intent

--- tests/Update/FileIOTest.elm
+++ tests/Update/FileIOTest.elm
@@ -10,81 +10,93 @@ suite : Test
 suite =
     describe "Update.FileIO"
         [ describe "exportToPPTX"
-            [ test "does not modify model" <|
-                \_ ->
-                    let
-                        model =
-                            initialModel
+            [ describe "when exporting a presentation"
+                [ test "should not modify the model" <|
+                    \_ ->
+                        let
+                            model =
+                                initialModel
 
-                        ( newModel, _ ) =
-                            Update.FileIO.exportToPPTX model
-                    in
-                    Expect.equal model newModel
+                            ( newModel, _ ) =
+                                Update.FileIO.exportToPPTX model
+                        in
+                        Expect.equal model newModel
+                ]
             ]
         , describe "importPPTXRequested"
-            [ test "does not modify model" <|
-                \_ ->
-                    let
-                        model =
-                            initialModel
+            [ describe "when requesting to import a PPTX file"
+                [ test "should not modify the model" <|
+                    \_ ->
+                        let
+                            model =
+                                initialModel
 
-                        ( newModel, _ ) =
-                            Update.FileIO.importPPTXRequested model
-                    in
-                    Expect.equal model newModel
+                            ( newModel, _ ) =
+                                Update.FileIO.importPPTXRequested model
+                        in
+                        Expect.equal model newModel
+                ]
             ]
         , describe "pptxImported"
-            [ test "loads valid presentation JSON" <|
-                \_ ->
-                    let
-                        model =
-                            initialModel
+            [ describe "given valid presentation JSON"
+                [ describe "when importing a PPTX file"
+                    [ test "should load the presentation data" <|
+                        \_ ->
+                            let
+                                model =
+                                    initialModel
 
-                        json =
-                            """{"title":"Imported","author":"Importer","created":"2025-01-01","slides":[{"content":"# Imported Slide","image":null}]}"""
+                                json =
+                                    """{"title":"Imported","author":"Importer","created":"2025-01-01","slides":[{"content":"# Imported Slide","image":null}]}"""
 
-                        ( newModel, _ ) =
-                            Update.FileIO.pptxImported json model
-                    in
-                    Expect.equal "Imported" newModel.presentation.title
-            , test "sets currentSlideIndex to 0" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | currentSlideIndex = 5 }
+                                ( newModel, _ ) =
+                                    Update.FileIO.pptxImported json model
+                            in
+                            Expect.equal "Imported" newModel.presentation.title
+                    , test "should reset currentSlideIndex to the first slide" <|
+                        \_ ->
+                            let
+                                model =
+                                    { initialModel | currentSlideIndex = 5 }
 
-                        json =
-                            """{"title":"Test","author":"Test","created":"2025-01-01","slides":[{"content":"# Test","image":null}]}"""
+                                json =
+                                    """{"title":"Test","author":"Test","created":"2025-01-01","slides":[{"content":"# Test","image":null}]}"""
 
-                        ( newModel, _ ) =
-                            Update.FileIO.pptxImported json model
-                    in
-                    Expect.equal 0 newModel.currentSlideIndex
-            , test "sets editingContent from first slide" <|
-                \_ ->
-                    let
-                        model =
-                            initialModel
+                                ( newModel, _ ) =
+                                    Update.FileIO.pptxImported json model
+                            in
+                            Expect.equal 0 newModel.currentSlideIndex
+                    , test "should set editingContent from the first slide" <|
+                        \_ ->
+                            let
+                                model =
+                                    initialModel
 
-                        json =
-                            """{"title":"Test","author":"Test","created":"2025-01-01","slides":[{"content":"# First","image":null}]}"""
+                                json =
+                                    """{"title":"Test","author":"Test","created":"2025-01-01","slides":[{"content":"# First","image":null}]}"""
 
-                        ( newModel, _ ) =
-                            Update.FileIO.pptxImported json model
-                    in
-                    Expect.equal "# First" newModel.editingContent
-            , test "ignores invalid JSON" <|
-                \_ ->
-                    let
-                        model =
-                            initialModel
+                                ( newModel, _ ) =
+                                    Update.FileIO.pptxImported json model
+                            in
+                            Expect.equal "# First" newModel.editingContent
+                    ]
+                ]
+            , describe "given invalid JSON"
+                [ describe "when attempting to import"
+                    [ test "should not modify the model" <|
+                        \_ ->
+                            let
+                                model =
+                                    initialModel
 
-                        invalidJson =
-                            "not valid json"
+                                invalidJson =
+                                    "not valid json"
 
-                        ( newModel, _ ) =
-                            Update.FileIO.pptxImported invalidJson model
-                    in
-                    Expect.equal model newModel
+                                ( newModel, _ ) =
+                                    Update.FileIO.pptxImported invalidJson model
+                            in
+                            Expect.equal model newModel
+                    ]
+                ]
             ]
         ]
