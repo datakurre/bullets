9029eeb0e7a4689ea37da9497f9af884a513b8b0
Asko Soukka <asko.soukka@iki.fi>
2025-12-17 12:42:14 +0200

    Refactor AccessibilityTest.elm to BDD style
    - Add nested describe blocks for given-when-then structure
    - Use 'should' pattern for test names
    - Group tests by accessibility features
    - Improves test readability and intent

--- tests/AccessibilityTest.elm
+++ tests/AccessibilityTest.elm
@@ -9,89 +9,105 @@ suite : Test
 suite =
     describe "Accessibility Announcements"
         [ describe "Live Region Announcements"
-            [ test "Initial announcement should be empty" <|
-                \_ ->
-                    initialModel.announcement
-                        |> Expect.equal ""
-            , test "Adding a slide should set announcement" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | announcement = "" }
+            [ describe "given the initial model"
+                [ test "should have an empty announcement" <|
+                    \_ ->
+                        initialModel.announcement
+                            |> Expect.equal ""
+                ]
+            , describe "when adding a slide"
+                [ test "should set an announcement" <|
+                    \_ ->
+                        let
+                            model =
+                                { initialModel | announcement = "" }
 
-                        updatedModel =
-                            updateWithAnnouncement AddSlide model
-                    in
-                    updatedModel.announcement
-                        |> Expect.notEqual ""
-            , test "Deleting a slide should set announcement" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | announcement = "" }
+                            updatedModel =
+                                updateWithAnnouncement AddSlide model
+                        in
+                        updatedModel.announcement
+                            |> Expect.notEqual ""
+                ]
+            , describe "when deleting a slide"
+                [ test "should set an announcement" <|
+                    \_ ->
+                        let
+                            model =
+                                { initialModel | announcement = "" }
 
-                        updatedModel =
-                            updateWithAnnouncement (DeleteSlide 0) model
-                    in
-                    updatedModel.announcement
-                        |> Expect.notEqual ""
-            , test "Navigating to next slide should announce slide number" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | announcement = "", currentSlideIndex = 0 }
+                            updatedModel =
+                                updateWithAnnouncement (DeleteSlide 0) model
+                        in
+                        updatedModel.announcement
+                            |> Expect.notEqual ""
+                ]
+            , describe "when navigating to the next slide"
+                [ test "should announce the slide number" <|
+                    \_ ->
+                        let
+                            model =
+                                { initialModel | announcement = "", currentSlideIndex = 0 }
 
-                        updatedModel =
-                            updateWithAnnouncement NextSlide model
-                    in
-                    updatedModel.announcement
-                        |> Expect.notEqual ""
-            , test "Entering presentation mode should set announcement" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | announcement = "" }
+                            updatedModel =
+                                updateWithAnnouncement NextSlide model
+                        in
+                        updatedModel.announcement
+                            |> Expect.notEqual ""
+                ]
+            , describe "when entering presentation mode"
+                [ test "should announce the mode change" <|
+                    \_ ->
+                        let
+                            model =
+                                { initialModel | announcement = "" }
 
-                        updatedModel =
-                            updateWithAnnouncement EnterPresentMode model
-                    in
-                    updatedModel.announcement
-                        |> Expect.equal "Entering presentation mode"
-            , test "Exiting presentation mode should set announcement" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | announcement = "" }
+                            updatedModel =
+                                updateWithAnnouncement EnterPresentMode model
+                        in
+                        updatedModel.announcement
+                            |> Expect.equal "Entering presentation mode"
+                ]
+            , describe "when exiting presentation mode"
+                [ test "should announce the mode change" <|
+                    \_ ->
+                        let
+                            model =
+                                { initialModel | announcement = "" }
 
-                        updatedModel =
-                            updateWithAnnouncement ExitPresentMode model
-                    in
-                    updatedModel.announcement
-                        |> Expect.equal "Exiting presentation mode"
+                            updatedModel =
+                                updateWithAnnouncement ExitPresentMode model
+                        in
+                        updatedModel.announcement
+                            |> Expect.equal "Exiting presentation mode"
+                ]
             ]
         , describe "Keyboard Slide Reordering"
-            [ test "MoveSlideUp should announce slide moved" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | announcement = "", currentSlideIndex = 1 }
+            [ describe "when moving a slide up"
+                [ test "should announce the slide movement" <|
+                    \_ ->
+                        let
+                            model =
+                                { initialModel | announcement = "", currentSlideIndex = 1 }
 
-                        updatedModel =
-                            updateWithAnnouncement (MoveSlideUp 1) model
-                    in
-                    updatedModel.announcement
-                        |> Expect.equal "Slide moved up"
-            , test "MoveSlideDown should announce slide moved" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | announcement = "", currentSlideIndex = 0 }
+                            updatedModel =
+                                updateWithAnnouncement (MoveSlideUp 1) model
+                        in
+                        updatedModel.announcement
+                            |> Expect.equal "Slide moved up"
+                ]
+            , describe "when moving a slide down"
+                [ test "should announce the slide movement" <|
+                    \_ ->
+                        let
+                            model =
+                                { initialModel | announcement = "", currentSlideIndex = 0 }
 
-                        updatedModel =
-                            updateWithAnnouncement (MoveSlideDown 0) model
-                    in
-                    updatedModel.announcement
-                        |> Expect.equal "Slide moved down"
+                            updatedModel =
+                                updateWithAnnouncement (MoveSlideDown 0) model
+                        in
+                        updatedModel.announcement
+                            |> Expect.equal "Slide moved down"
+                ]
             ]
         ]
 
