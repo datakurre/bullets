6ccbc4f9bc4703eb253cd8d5ce85c3187b725334
Asko Soukka <asko.soukka@iki.fi>
2025-12-17 12:20:48 +0200

    Refactor Update/UITest.elm to BDD style
    - Add nested describe blocks for given-when-then structure
    - Use 'should' pattern for test names
    - Group tests by UI state changes and language management
    - Improves test readability and intent

--- tests/Update/UITest.elm
+++ tests/Update/UITest.elm
@@ -11,109 +11,137 @@ suite : Test
 suite =
     describe "Update.UI"
         [ describe "toggleHelpDialog"
-            [ test "toggles from false to true" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | showHelpDialog = False }
+            [ describe "given the help dialog is hidden"
+                [ describe "when toggling the help dialog"
+                    [ test "should show the help dialog" <|
+                        \_ ->
+                            let
+                                model =
+                                    { initialModel | showHelpDialog = False }
 
-                        ( newModel, _ ) =
-                            Update.UI.toggleHelpDialog model
-                    in
-                    Expect.equal True newModel.showHelpDialog
-            , test "toggles from true to false" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | showHelpDialog = True }
+                                ( newModel, _ ) =
+                                    Update.UI.toggleHelpDialog model
+                            in
+                            Expect.equal True newModel.showHelpDialog
+                    ]
+                ]
+            , describe "given the help dialog is visible"
+                [ describe "when toggling the help dialog"
+                    [ test "should hide the help dialog" <|
+                        \_ ->
+                            let
+                                model =
+                                    { initialModel | showHelpDialog = True }
 
-                        ( newModel, _ ) =
-                            Update.UI.toggleHelpDialog model
-                    in
-                    Expect.equal False newModel.showHelpDialog
+                                ( newModel, _ ) =
+                                    Update.UI.toggleHelpDialog model
+                            in
+                            Expect.equal False newModel.showHelpDialog
+                    ]
+                ]
             ]
         , describe "textareaFocused"
-            [ test "sets isTextareaFocused to True" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | isTextareaFocused = False }
+            [ describe "when the textarea receives focus"
+                [ test "should set isTextareaFocused to true" <|
+                    \_ ->
+                        let
+                            model =
+                                { initialModel | isTextareaFocused = False }
 
-                        ( newModel, _ ) =
-                            Update.UI.textareaFocused model
-                    in
-                    Expect.equal True newModel.isTextareaFocused
+                            ( newModel, _ ) =
+                                Update.UI.textareaFocused model
+                        in
+                        Expect.equal True newModel.isTextareaFocused
+                ]
             ]
         , describe "textareaBlurred"
-            [ test "sets isTextareaFocused to False" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | isTextareaFocused = True }
+            [ describe "when the textarea loses focus"
+                [ test "should set isTextareaFocused to false" <|
+                    \_ ->
+                        let
+                            model =
+                                { initialModel | isTextareaFocused = True }
 
-                        ( newModel, _ ) =
-                            Update.UI.textareaBlurred model
-                    in
-                    Expect.equal False newModel.isTextareaFocused
+                            ( newModel, _ ) =
+                                Update.UI.textareaBlurred model
+                        in
+                        Expect.equal False newModel.isTextareaFocused
+                ]
             ]
         , describe "changeLanguage"
-            [ test "changes language from English to Finnish" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | language = English }
+            [ describe "given the current language is English"
+                [ describe "when changing to Finnish"
+                    [ test "should update the language to Finnish" <|
+                        \_ ->
+                            let
+                                model =
+                                    { initialModel | language = English }
 
-                        ( newModel, _ ) =
-                            Update.UI.changeLanguage Finnish model
-                    in
-                    Expect.equal Finnish newModel.language
-            , test "changes language from Finnish to English" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | language = Finnish }
+                                ( newModel, _ ) =
+                                    Update.UI.changeLanguage Finnish model
+                            in
+                            Expect.equal Finnish newModel.language
+                    ]
+                ]
+            , describe "given the current language is Finnish"
+                [ describe "when changing to English"
+                    [ test "should update the language to English" <|
+                        \_ ->
+                            let
+                                model =
+                                    { initialModel | language = Finnish }
 
-                        ( newModel, _ ) =
-                            Update.UI.changeLanguage English model
-                    in
-                    Expect.equal English newModel.language
+                                ( newModel, _ ) =
+                                    Update.UI.changeLanguage English model
+                            in
+                            Expect.equal English newModel.language
+                    ]
+                ]
             ]
         , describe "languageLoaded"
-            [ test "loads Finnish language preference" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | language = English }
+            [ describe "when loading language preference from storage"
+                [ describe "given valid Finnish preference"
+                    [ test "should set language to Finnish" <|
+                        \_ ->
+                            let
+                                model =
+                                    { initialModel | language = English }
 
-                        jsonString =
-                            "\"fi\""
+                                jsonString =
+                                    "\"fi\""
 
-                        ( newModel, _ ) =
-                            Update.UI.languageLoaded jsonString model
-                    in
-                    Expect.equal Finnish newModel.language
-            , test "loads English language preference" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | language = Finnish }
+                                ( newModel, _ ) =
+                                    Update.UI.languageLoaded jsonString model
+                            in
+                            Expect.equal Finnish newModel.language
+                    ]
+                , describe "given valid English preference"
+                    [ test "should set language to English" <|
+                        \_ ->
+                            let
+                                model =
+                                    { initialModel | language = Finnish }
 
-                        jsonString =
-                            "\"en\""
+                                jsonString =
+                                    "\"en\""
 
-                        ( newModel, _ ) =
-                            Update.UI.languageLoaded jsonString model
-                    in
-                    Expect.equal English newModel.language
-            , test "ignores invalid JSON" <|
-                \_ ->
-                    let
-                        model =
-                            { initialModel | language = English }
+                                ( newModel, _ ) =
+                                    Update.UI.languageLoaded jsonString model
+                            in
+                            Expect.equal English newModel.language
+                    ]
+                , describe "given invalid JSON"
+                    [ test "should keep the current language" <|
+                        \_ ->
+                            let
+                                model =
+                                    { initialModel | language = English }
 
-                        ( newModel, _ ) =
-                            Update.UI.languageLoaded "invalid" model
-                    in
-                    Expect.equal English newModel.language
+                                ( newModel, _ ) =
+                                    Update.UI.languageLoaded "invalid" model
+                            in
+                            Expect.equal English newModel.language
+                    ]
+                ]
             ]
         ]
